d49e6df05b6768955f01096dda56e3b5ab4e7432
ManOfFeathers <griffintmyers@gmail.com>
2017-06-03 16:15:43 -0600

    Improved borders, added stretch

--- ImageProcessingToolkit.py
+++ ImageProcessingToolkit.py
@@ -20,7 +20,7 @@
 from graphics import GraphicsImage, GraphicsWindow
 from ColorUtils import *
 
-SHOULD_DISPLAY_IMAGE = False
+SHOULD_DISPLAY_IMAGE = True
 
 ##  Template
 # Use this function as a template for solving the problems below.
@@ -53,36 +53,47 @@ def template(filename):
 # of a given color around an image.  Parameters:  color, width
 
 def border(filename, color, border_width):
-    image = GraphicsImage(filename)
-    width = image.width()
-    height = image.height()
-    for row in range(height):
-        for col in range(width):
-            # get the current pixel
-            red = image.getRed(row,col)
-            green = image.getGreen(row,col)
-            blue = image.getBlue(row,col)
+    orig_image = GraphicsImage(filename)
+    orig_width = orig_image.width()
+    orig_height = orig_image.height()
 
-            # get border color
-            (b_red, b_green, b_blue) = hex_to_rgb(color)
+    new_width = orig_width + 2 * border_width
+    new_height = orig_height + 2 * border_width
+    new_image = GraphicsImage(new_width,new_height)
+    for row in range(0,new_height):
+        for col in range(0,new_width):
 
             # set booleans for conditions
             isTopBorder = row < border_width
-            isBottomBorder = row >= height - border_width
+            isBottomBorder = row >= new_height - border_width
             isLeftBorder = col < border_width
-            isRightBorder = col >= width - border_width
+            isRightBorder = col >= new_width - border_width
+
+             # get border color
+            (b_red, b_green, b_blue) = hex_to_rgb(color)
 
             if (isTopBorder or isBottomBorder or isLeftBorder or isRightBorder):
-                image.setPixel(row,col,b_red,b_green,b_blue)  # set to border color
+                new_image.setPixel(row,col,b_red,b_green,b_blue)  # set to border color
             else:
-                image.setPixel(row,col,red,green,blue)  # set to original color
+                orig_row = row - border_width
+                orig_col = col - border_width
+
+                # get the current pixel
+                red = orig_image.getRed(orig_row,orig_col)
+                green = orig_image.getGreen(orig_row,orig_col)
+                blue = orig_image.getBlue(orig_row,orig_col)
+
+                new_image.setPixel(row,col,red,green,blue)  # set to original color
+
+
+                
 
     # save and display new image
     if (SHOULD_DISPLAY_IMAGE):
         win = GraphicsWindow()
         canvas = win.canvas()
-        canvas.drawImage(image)
-    image.save("border-"+filename)
+        canvas.drawImage(new_image)
+    new_image.save("border-"+filename)
 
 ##  Problem 2
 # Add a function to the image processing toolkit that reduces an image by half,
@@ -173,6 +184,29 @@ def sideClone(filename):
         canvas.drawImage(newImage)
     newImage.save("side-clone"+filename)
 
+def downClone(filename):
+    originalImage = GraphicsImage(filename)
+    originalWidth = originalImage.width()
+    originalHeight = originalImage.height()
+
+    newWidth = originalImage.width()
+    newHeight = originalImage.height() * 2
+    newImage = GraphicsImage(newWidth,newHeight)
+
+    for row in range (0,originalHeight):
+        for col in range (0,originalWidth):
+            red = originalImage.getRed(row,col)
+            green = originalImage.getGreen(row,col)
+            blue = originalImage.getBlue(row,col)
+            newImage.setPixel(row,col,red,green,blue)
+            newImage.setPixel(row+originalHeight,col,red,green,blue)
+
+    if (SHOULD_DISPLAY_IMAGE):
+        win = GraphicsWindow()
+        canvas = win.canvas()
+        canvas.drawImage(newImage)
+    newImage.save("down-clone"+filename)
+
 ##  Problem 5
 # Add a function to the image processing toolkit that changes an
 # image to grayscale, using a darkness ratio calculated by the following
@@ -207,7 +241,65 @@ def greyscale(filename):
 # In the documentation, explain your algorithm for selecting the color.
 # It should not be selected randomly.
 
-#def newBorder(filename):
+# The border color is based on an algorithm that finds the average value
+# of red, green, and blue in each pixel, and uses each average to create
+# its own color. The function achieves this by finding the average values
+# for every pixel in a for loop.
+# -Griffin
+
+def smartBorder(filename, border_width):
+    orig_image = GraphicsImage(filename)
+    orig_width = orig_image.width()
+    orig_height = orig_image.height()
+
+    new_width = orig_width + 2 * border_width
+    new_height = orig_height + 2 * border_width
+    new_image = GraphicsImage(new_width,new_height)
+
+    red = 0
+    blue = 0
+    green = 0
+
+    for row in range(orig_height):
+        for col in range(orig_width):
+            red += orig_image.getRed(row, col)
+            green += orig_image.getGreen(row, col)
+            blue += orig_image.getBlue(row, col)
+
+    avg_red = red // (orig_width * orig_height)
+    avg_green = green // (orig_width * orig_height)
+    avg_blue = blue // (orig_width * orig_height)
+
+    for row in range(0, new_height):
+        for col in range(0, new_width):
+
+            # set booleans for conditions
+            isTopBorder = row < border_width
+            isBottomBorder = row >= new_height - border_width
+            isLeftBorder = col < border_width
+            isRightBorder = col >= new_width - border_width
+
+            if (isTopBorder or isBottomBorder or isLeftBorder or isRightBorder):
+                new_image.setPixel(row,col,avg_red,avg_green,avg_blue)  # set to border color
+            else:
+                orig_row = row - border_width
+                orig_col = col - border_width
+
+                # get the current pixel
+                red = orig_image.getRed(orig_row,orig_col)
+                green = orig_image.getGreen(orig_row,orig_col)
+                blue = orig_image.getBlue(orig_row,orig_col)
+
+                new_image.setPixel(row,col,red,green,blue)  # set to original color
+
+    # save and display new image
+    if (SHOULD_DISPLAY_IMAGE):
+        win = GraphicsWindow()
+        canvas = win.canvas()
+        canvas.drawImage(new_image)
+    new_image.save("smart-border-"+filename)
+
+            
 
 ##  Problem 7
 # Modify the code for problems 2 and 3 to include a scale parameter that
@@ -222,6 +314,35 @@ def greyscale(filename):
 # produces a single image replicating the original three copies wide and
 # two copies tall.
 
+def smartClone(filename, side, down):
+    if not type(side) == int or not type(down) == int:
+        print("Side and down need to be integers.")
+
+    originalImage = GraphicsImage(filename)
+    originalWidth = originalImage.width()
+    originalHeight = originalImage.height()
+
+    newWidth = originalImage.width() * side
+    newHeight = originalImage.height() * down
+    newImage = GraphicsImage(newWidth,newHeight)
+
+    for row in range (0,originalHeight):
+        for col in range (0,originalWidth):
+            red = originalImage.getRed(row,col)
+            green = originalImage.getGreen(row,col)
+            blue = originalImage.getBlue(row,col)
+            for img_row in range (0, down):
+                newImage.setPixel(row+img_row*originalHeight,col,red,green,blue)
+            for img_col in range(0, side):
+                newImage.setPixel(row,col+img_col*originalWidth,red,green,blue)
+            
+    if (SHOULD_DISPLAY_IMAGE):
+        win = GraphicsWindow()
+        canvas = win.canvas()
+        canvas.drawImage(newImage)
+    newImage.save("smart-clone"+filename)
+    
+
 ##  Problem 9
 # Modify the code from problem 5 to allow for other photographic print toning,
 # such as alternate-ratio greyscale, pure black-and-white (with threshold
@@ -231,3 +352,28 @@ def greyscale(filename):
 ##  Problem 10
 # Create a photo manipulation effect of your own design.  Ideas can include
 # stretch, blur, mosaic, 8-bit, etc...
+
+def stretch(filename, x, y):
+    originalImage = GraphicsImage(filename)
+    originalWidth = originalImage.width()
+    originalHeight = originalImage.height()
+
+    newWidth = int(originalImage.width() * x)
+    newHeight = int(originalImage.height() * y)
+    newImage = GraphicsImage(newWidth,newHeight)
+
+    for row in range (0,newHeight):
+        for col in range (0,newWidth):
+            orig_row = int(row//y)
+            orig_col = int(col//x)
+
+            red = originalImage.getRed(orig_row,orig_col)
+            green = originalImage.getGreen(orig_row,orig_col)
+            blue = originalImage.getBlue(orig_row,orig_col)
+            newImage.setPixel(row,col,red,green,blue) # a value of 1 is default image
+
+    if (SHOULD_DISPLAY_IMAGE):
+        win = GraphicsWindow()
+        canvas = win.canvas()
+        canvas.drawImage(newImage)
+    newImage.save("stretch-"+filename)
--- ImageProcessingToolkitTester.py
+++ ImageProcessingToolkitTester.py
@@ -20,22 +20,35 @@
 #  receive permission from the author(s).                                 #
 ###########################################################################
 from ImageProcessingToolkit import *
+import math
 
-template("arrow.gif")
-template("jellyfish.gif")
-
-# 1
-border("arrow.gif", "#FFFF00", 5)
-border("jellyfish.gif", "#FFFF00", 5)
-# 2
-half("arrow.gif")
-half("jellyfish.gif")
-# 3
-double("arrow.gif")
-double("jellyfish.gif")
-# 4
-sideClone("arrow.gif")
-sideClone("jellyfish.gif")
-# 5
-greyscale("arrow.gif")
-greyscale("jellyfish.gif")
+##template("arrow.gif")
+##template("jellyfish.gif")
+##
+### 1
+##border("arrow.gif", "#FFFF00", 50)
+##border("jellyfish.gif", "#FFFF00", 5)
+### 2
+##half("arrow.gif")
+##half("jellyfish.gif")
+### 3
+##double("arrow.gif")
+##double("jellyfish.gif")
+### 4
+##sideClone("arrow.gif")
+##sideClone("jellyfish.gif")
+##
+##downClone("arrow.gif")
+##downClone("jellyfish.gif")
+### 5
+##greyscale("arrow.gif")
+##greyscale("jellyfish.gif")
+### 6
+##smartBorder("arrow.gif",10)
+##smartBorder("jellyfish.gif",100)
+### 8
+##smartClone("arrow.gif",3,2)
+##smartClone("jellyfish.gif",3,2)
+### 10
+##stretch("arrow.gif",math.pi,1)
+##stretch("jellyfish.gif", 0.5,1)
