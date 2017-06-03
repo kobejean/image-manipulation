0693fd9234acf630a7d55aaadb3361ecc77672e7
kobejean <kobejean@me.com>
2017-06-03 12:27:02 -0600

    Added ColorUtils.py

---
+++ ColorUtils.py
@@ -0,0 +1,11 @@
+# code from: https://stackoverflow.com/questions/214359/converting-hex-color-to-rgb-and-vice-versa
+
+def hex_to_rgb(value):
+    """Return (red, green, blue) for the color given as #rrggbb."""
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_hex(red, green, blue):
+    """Return color as #rrggbb for the given color values."""
+    return '#%02x%02x%02x' % (red, green, blue)
