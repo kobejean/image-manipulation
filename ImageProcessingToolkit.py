fb85ee09501528429d25323036972c66175af018
kobejean <kobejean@me.com>
2017-06-03 21:23:09 -0600

    Merge branch 'jean' of https://github.com/kobejean/image-manipulation into jean

--- ImageProcessingToolkit.py
+++ ImageProcessingToolkit.py
@@ -277,6 +277,7 @@ def smartBorder(filename, border_width):
                 orig_col = col - border_width
 
                 # get the current pixel
+
                 (red, green, blue) = orig_image.getPixel(orig_row,orig_col)
 
                 new_image.setPixel(row,col,red,green,blue)  # set to original color
