1984637afe7caac8bd57cf5e262cd6ed27cb3808
kobejean <kobejean@me.com>
2017-06-10 14:47:47 -0600

    Merge Griffin

--- img_manip_test.py
+++ img_manip_test.py
@@ -23,58 +23,63 @@
 from img_manip import *
 import math
 
-template("griffin.gif")
-template("jean.gif")
-template("totoro.gif")
-
-# problem 1
-border("griffin.gif", "#00FF00", 30)
-border("jean.gif", "#FF0000", 30)
-border("totoro.gif", "#0000FF", 100)
-# problem 2
-half("griffin.gif")
-half("jean.gif")
-half("totoro.gif")
-# problem 3
-double("griffin.gif")
-double("jean.gif")
-double("totoro.gif")
-# problem 4
-sideClone("griffin.gif")
-sideClone("jean.gif")
-sideClone("totoro.gif")
-downClone("griffin.gif")
-downClone("jean.gif")
-downClone("totoro.gif")
-# problem 5
-greyscale("griffin.gif")
-greyscale("jean.gif")
-greyscale("totoro.gif")
-# problem 6
-smartBorder("griffin.gif",30)
-smartBorder("jean.gif",30)
-smartBorder("totoro.gif",100)
-# problem 7
-# scale_factor = float(input("SCALE FACTOR:"))
-scale_factor = 0.25
-scale("griffin.gif", scale_factor)
-scale("jean.gif", scale_factor)
-scale("totoro.gif", scale_factor)
-# problem 8
-smartClone("griffin.gif",3,2)
-smartClone("jean.gif",3,2)
-smartClone("totoro.gif",3,2)
-tile("griffin.gif",3,2)
-tile("jean.gif",3,2)
-tile("totoro.gif",3,2)
-# problem 9
-colorSubstitution("griffin.gif", 0.55, 0.15, 1.0)
-colorSubstitution("jean.gif", 0.5, 0.5, 1.0)
-colorSubstitution("totoro.gif", 0.5, 0.5, 1.0)
-contrastBorder("griffin.gif",30)
-contrastBorder("jean.gif",30)
-contrastBorder("totoro.gif",100)
-# problem 10
-stretch("griffin.gif",math.pi,1)
-stretch("jean.gif", 0.5,1)
-stretch("totoro.gif",0.5,0.75)
+# template("griffin.gif")
+# template("jean.gif")
+# template("totoro.gif")
+#
+# # problem 1
+# border("griffin.gif", "#00FF00", 30)
+# border("jean.gif", "#FF0000", 30)
+# border("totoro.gif", "#0000FF", 100)
+# # problem 2
+# half("griffin.gif")
+# half("jean.gif")
+# half("totoro.gif")
+# # problem 3
+# double("griffin.gif")
+# double("jean.gif")
+# double("totoro.gif")
+# # problem 4
+# sideClone("griffin.gif")
+# sideClone("jean.gif")
+# sideClone("totoro.gif")
+# downClone("griffin.gif")
+# downClone("jean.gif")
+# downClone("totoro.gif")
+# # problem 5
+# greyscale("griffin.gif")
+# greyscale("jean.gif")
+# greyscale("totoro.gif")
+# # problem 6
+# smartBorder("griffin.gif",30)
+# smartBorder("jean.gif",30)
+# smartBorder("totoro.gif",100)
+# # problem 7
+# # scale_factor = float(input("SCALE FACTOR:"))
+# scale_factor = 0.25
+# scale("griffin.gif", scale_factor)
+# scale("jean.gif", scale_factor)
+# scale("totoro.gif", scale_factor)
+# # problem 8
+# smartClone("griffin.gif",3,2)
+# smartClone("jean.gif",3,2)
+# smartClone("totoro.gif",3,2)
+# tile("griffin.gif",3,2)
+# tile("jean.gif",3,2)
+# tile("totoro.gif",3,2)
+# # problem 9
+# colorSubstitution("griffin.gif", 0.55, 0.15, 1.0)
+# colorSubstitution("jean.gif", 0.5, 0.5, 1.0)
+# colorSubstitution("totoro.gif", 0.5, 0.5, 1.0)
+# contrastBorder("griffin.gif",30)
+# contrastBorder("jean.gif",30)
+# contrastBorder("totoro.gif",100)
+# # problem 10
+# stretch("griffin.gif",math.pi,1)
+# stretch("jean.gif", 0.5,1)
+# stretch("totoro.gif",0.5,0.75)
+# problem 11
+# harrisShutter("jellyfish.gif",0,20)
+# # harrisShutter("griffin.gif",0,20)
+# # harrisShutter("jean.gif",20,20)
+harrisShutter("totoro.gif",50,0)
